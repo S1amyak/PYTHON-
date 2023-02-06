027589657f77bc2f16faab08323beae7ec8a1d2c
S1amyak <115889693+S1amyak@users.noreply.github.com>
2023-02-06 23:08:30 +0530

    Update sort array in ascending.py

--- sort array in ascending.py
+++ sort array in ascending.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 li = [int(a) for a in input("").split(" ")]
 n = len(li)
 for i in range(n):
@@ -6,12 +5,3 @@ for i in range(n):
         if li[j] > li[j+1]:
             li[j],li[j+1] = li[j+1],li[j]
 print(li)
-=======
-li = [int(a) for a in input("").split(" ")]
-n = len(li)
-for i in range(n):
-    for j in range(0,n-i-1):
-        if li[j] > li[j+1]:
-            li[j],li[j+1] = li[j+1],li[j]
-print(li)
->>>>>>> f4e24b651353780523b041fd75d2596ff535ad11
